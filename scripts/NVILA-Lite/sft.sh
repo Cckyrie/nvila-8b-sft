125dc164aa43e8d30c8af72f7a779587f4726989
Cckyrie <144601107+Cckyrie@users.noreply.github.com>
2025-05-12 09:53:49 +0800

    Update sft.sh

--- scripts/NVILA-Lite/sft.sh
+++ scripts/NVILA-Lite/sft.sh
@@ -46,7 +46,7 @@ torchrun \
         --save_strategy steps \
         --save_steps 100 \
         --save_total_limit 1 \
-        --learning_rate 2e-5 \
+        --learning_rate 2e-6 \
         --weight_decay 0. \
         --warmup_ratio 0.03 \
         --lr_scheduler_type cosine \
